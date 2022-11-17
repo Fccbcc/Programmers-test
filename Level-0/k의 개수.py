488717766ddef2cb6f9e2f523c8d9f834102f05f
Fccbcc <118347315+Fccbcc@users.noreply.github.com>
2022-11-18 00:26:00 +0900

    Update k의 개수.py

--- Level-0/k의 개수.py
+++ Level-0/k의 개수.py
@@ -11,3 +11,17 @@ def solution(i, j, k):
     return answer
   
 #해설 생략(합성수 찾기와 유사)
+
+#다른 풀이
+
+def solution(i, j, k):
+    answer = sum([ str(i).count(str(k)) for i in range(i,j+1)])
+    return answer
+
+
+def solution(i, j, k):
+    answer = 0
+    for num in range(i, j + 1):
+        if str(k) in str(num):
+            answer += str(num).count(str(k))
+    return answer
