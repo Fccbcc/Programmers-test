a9c10aee1568116482015ce367c4368987582610
Fccbcc <118347315+Fccbcc@users.noreply.github.com>
2022-11-17 23:35:57 +0900

    Update 369게임.py

--- Level-0/369게임.py
+++ Level-0/369게임.py
@@ -10,3 +10,14 @@ def solution(order):
 
 # 해설
 # 배수에서 주의해야하는 부분.. 0을 나누면 0이다
+
+# 다른 해설
+
+def solution(order):
+    result = 0
+
+    for i in range(3, 10, 3):
+        result += str(order).count(str(i))
+
+    return result
+#369 게임에서 369만 쓰면 되는 것!
